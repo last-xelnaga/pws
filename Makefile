f9929a5864397b9fc52aad8f77f65ba363ef3df5
Vadims Tihomirovs <asiris.mail@gmail.com>
2017-05-03 23:42:20 +0200

    relay setup

--- Makefile
+++ Makefile
@@ -21,19 +21,19 @@ OBJ = $(SRC:%.cpp=$(OBJDIR)/%.o)
 $(OBJDIR)/%.o: %.cpp
 	$(CC) $(CFLAGS) $(INC) -c -o $@ $<
 
+.PHONY: all clean
+
+all: $(TARGET)
+
 $(OBJDIR):
 	mkdir -p $(OBJDIR)
 
 $(TARGET): $(OBJDIR) $(OBJ)
-
-.PHONY: all clean
-
-all: $(TARGET)
+	$(CC) -o $@ $(OBJ) $(LIB)
 
 clean:
 	rm -rf $(OBJDIR) $(TARGET)
 
 $(OBJDIR)/main.o: Makefile
-
 
 # valgrind --leak-check=full --show-leak-kinds=all ./shp_client
--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <wiringPi.h>
+
+#define RELAY_PIN     1
+
+
+void relay_switch_on (
+        void)
+{
+    digitalWrite (RELAY_PIN, 1);
+}
+
+void relay_switch_off (
+        void)
+{
+    digitalWrite (RELAY_PIN, 0);
+}
 
 int main (
         void)
 {
-    printf ("hello world\n");
+    if (geteuid () != 0)
+    {
+        printf ("need to be root to run. exit");
+        return -1;
+    }
+
+    if (wiringPiSetup () == -1)
+    {
+        printf ("board setup has failed. exit");
+        return -1;
+    }
+
+    // setup relay
+    pinMode (RELAY_PIN, OUTPUT);
+    relay_switch_off ();
+
+    // setup soil sensor
+    
+
 
     return 0;
 }
